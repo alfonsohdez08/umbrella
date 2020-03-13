3d914948b07455edd08850fd6de9df91391175cc
Alfonso Hernandez <alfonsohdez08@hotmail.com>
2020-03-12 22:52:04 -0400

    add column comments

--- src/Umbrella/Column.cs
+++ src/Umbrella/Column.cs
@@ -4,12 +4,34 @@ using System.Text;
 
 namespace Umbrella
 {
+    /// <summary>
+    /// Column inferred/discovered by the columns mapping process.
+    /// </summary>
     internal class Column
     {
+        /// <summary>
+        /// Name of the DataColumn.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Data type of the DataColumn.
+        /// </summary>
         public Type DataType { get; set; }
+
+        /// <summary>
+        /// Denotes whether the DataColumn allows NULL or not.
+        /// </summary>
         public bool IsNullable { get; set; }
+
+        /// <summary>
+        /// A compiled expression that maps the data into the DataColumn when creating DataRows.
+        /// </summary>
         public Delegate Mapper { get; set; }
+
+        /// <summary>
+        /// A flag that says when the mapper function/delegate is parameterless.
+        /// </summary>
         public bool IsMapperParameterless { get; set; }
     }
 }
